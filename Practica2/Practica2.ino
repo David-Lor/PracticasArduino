6d68d553e5c0fb447426f9d2fcb8c4c58d82251f
EnforcerZhukov <davidl.eco@gmail.com>
2018-04-18 13:37:37 +0200

    Actualizado sketch, funcional

--- Practica2/Practica2.ino
+++ Practica2/Practica2.ino
@@ -1,5 +1,5 @@
 #define LEN_LED 3
-int LED_PIN[3] = {2,3,4};
+int LED_PIN[LEN_LED] = {2,3,4};
 const int DELAY = 300;
 
 void setup() {
@@ -7,18 +7,13 @@ void setup() {
     pinMode(LED_PIN[i], OUTPUT);
     digitalWrite(LED_PIN[i], LOW);
   }
-  Serial.begin(9600);
 }
 
 void loop() {
   for (int i=0; i<LEN_LED; i++) {
     digitalWrite(LED_PIN[i], HIGH);
-    Serial.print("Enciendo ");
-    Serial.println(LED_PIN[i]);
     delay(DELAY);
     digitalWrite(LED_PIN[i], LOW);
-    Serial.print("Apago ");
-    Serial.println(LED_PIN[i]);
     delay(DELAY);
   }
 }
