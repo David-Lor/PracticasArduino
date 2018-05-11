561cc20c23f734a73f81de01cb556135722b4937
EnforcerZhukov <davidl.eco@gmail.com>
2018-05-11 12:29:20 +0200

    comentarios en código

--- Practica5B-ClapSwitch4LEDs/ClapSwitch4LEDs.ino
+++ Practica5B-ClapSwitch4LEDs/ClapSwitch4LEDs.ino
@@ -1,14 +1,17 @@
 
-#define TIEMPO_CLAP 1500
+#define TIEMPO_CLAP 1500 //Tiempo que debe pasar para procesar las palmadas detectadas (1.5seg)
 
+//Pinout
 const int pinMicro = 6;
-const int pinLed[4] = {2,3,4,5};
-bool led[4] = {false,false,false,false};
-int contador = 0;
-unsigned long lastClap = 0;
+const int pinLed[4] = {2, 3, 4, 5};
+
+//Variables globales
+bool led[4] = {false, false, false, false}; //estados de los LEDs
+unsigned int contador = 0; //contador de palmadas
+unsigned long lastClap = 0; //instante de la última palmada
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); //debug
   pinMode(pinMicro,INPUT); //inicializar micro input
   for (int i=0; i<4; i++) { //iterar todos los LED
     pinMode(pinLed[i], OUTPUT); //inicializar cada LED output
@@ -17,34 +20,34 @@ void setup() {
 
 void actuarLED() {
   if (contador > 1 && contador < 7) {
-    if (contador == 6) {
+    if (contador == 6) { //6 palmadas: reiniciar todos los LED
       for (int i=0; i<4; i++) {
         digitalWrite(pinLed[i], LOW);
         led[i] = false;
       }
-    } else {
+    } else { //2~5 palmadas: cambiar estado de un sólo LED
       int ledIndex = contador - 2;
       bool ledNow = !led[ledIndex];
       digitalWrite(pinLed[ledIndex], ledNow);
       led[ledIndex] = ledNow;
-      Serial.print("LED #");
-      Serial.print(ledIndex);
-      Serial.print(" -> ");
-      Serial.println(ledNow);
+      Serial.print("LED #"); //debug
+      Serial.print(ledIndex); //debug
+      Serial.print(" -> "); //debug
+      Serial.println(ledNow); //debug
     }
   }
-  contador = 0;
+  contador = 0; //reiniciar contador
 }
 
 void loop() {
 
   unsigned long currentMillis = millis();
   
-  if (digitalRead(pinMicro)) {
-    lastClap = currentMillis;
-    contador++;
-    Serial.print("Contador: ");
-    Serial.println(contador);
+  if (digitalRead(pinMicro)) { //palmada detectada
+    lastClap = currentMillis; //guardar tiempo última palmada
+    contador++; //actualizar contador
+    Serial.print("Contador: "); //debug
+    Serial.println(contador); //debug
     delay(200);
   }
 
